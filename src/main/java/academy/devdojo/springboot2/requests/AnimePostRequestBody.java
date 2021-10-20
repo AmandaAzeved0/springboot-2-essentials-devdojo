1ed75c1ca81e8a09a0bef7df32c19028d50c2a40
Amanda Nascimento <amanda.nascimento@cinbesa.com.br>
2021-10-20 15:04:09 -0300

    Initial commit

---
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -0,0 +1,14 @@
+package academy.devdojo.springboot2.requests;
+
+
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class AnimePostRequestBody {
+
+    @NotEmpty(message= "anime name cannot be empty")
+    private String name;
+}
